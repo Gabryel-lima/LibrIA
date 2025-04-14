8d3018a27b99b44405a8ae9ee24dee1ea021137e
Gabryel-lima <gabbryellimasi@gmail.com>
2025-04-14 14:18:46 -0300

    Add Transformer model implementation and configuration; update headers 1 in Python files

--- src/conf.py
+++ src/conf.py
@@ -25,30 +25,8 @@ class Config_Img_Classifier:
     # Paths
     MODEL_DIR = os.path.join(BASE_DIR, "./saved")
     BEST_MODEL = os.path.join(MODEL_DIR, "classification.pth")
-    
-class Config_Transformer:
-    # model parameter setting
-    batch_size = 128
-    max_len = 256
-    d_model = 512
-    n_layers = 6
-    n_heads = 8
-    ffn_hidden = 2048
-    drop_prob = 0.1
-
-    # optimizer parameter setting
-    init_lr = 1e-5
-    factor = 0.9
-    adam_eps = 5e-9
-    patience = 10
-    warmup = 100
-    epoch = 1000
-    clip = 1.0
-    weight_decay = 5e-4
-    inf = float('inf')
 
 __all__ = [
     '__DEVICE__',
     'Config_Img_Classifier',
-    'Config_Transformer'
 ]
